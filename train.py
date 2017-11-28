2ccad2ebdc9aa3f7e80f4101c551454432f4a163
Panamka <evazavorina@gmail.com>
2017-11-29 00:22:54 +0300

    sled change

--- train.py
+++ train.py
@@ -47,3 +47,58 @@ def plotmy3d(c, name):
     fig = go.Figure(data=data, layout=layout)
     py.iplot(fig)
 plotmy3d(X_band_1[844,:,:], 'iceberg')
+
+
+import keras 
+from matplotlib import pyplot
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout
+from keras.layers import GlobalMaxPooling2D
+from keras.models import Model
+
+def getModel():
+    gmodel=Sequential()
+    #Conv Layer 1
+    gmodel.add(Conv2D(64, kernel_size=(3, 3),activation='relu', input_shape=(75, 75, 3)))
+    gmodel.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    gmodel.add(Dropout(0.2))
+
+    #Conv Layer 2
+    gmodel.add(Conv2D(128, kernel_size=(3, 3), activation='relu' ))
+    gmodel.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    gmodel.add(Dropout(0.2))
+     #Conv Layer 3
+    gmodel.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    gmodel.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    gmodel.add(Dropout(0.2))
+
+    #Conv Layer 4
+    gmodel.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    gmodel.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    gmodel.add(Dropout(0.2))
+    
+    #Sosdaem 1D vektor
+    gmodel.add(Flatten())
+    
+    from keras.optimizers import Adam
+     #Dense Layers
+    gmodel.add(Dense(512))
+    gmodel.add(Activation('relu'))
+    gmodel.add(Dropout(0.2))
+
+    #Dense Layer 2
+    gmodel.add(Dense(256))
+    gmodel.add(Activation('relu'))
+    gmodel.add(Dropout(0.2))
+
+    #Sigmoid Layer
+    gmodel.add(Dense(1))
+    gmodel.add(Activation('sigmoid'))
+
+    mypotim=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    gmodel.compile(loss='binary_crossentropy',
+                  optimizer=mypotim,
+                  metrics=['accuracy'])
+    gmodel.summary()
+    return gmodel
